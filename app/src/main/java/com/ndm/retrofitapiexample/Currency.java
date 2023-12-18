eff2e305fdeb823a7745843aa70544a4eb159c62
ADMIN <manhdz22pl@gmail.com>
2023-12-18 21:34:25 +0700

    hello

---
+++ app/src/main/java/com/ndm/retrofitapiexample/Currency.java
@@ -0,0 +1,5 @@
+package com.ndm.retrofitapiexample;
+
+public class Currency {
+    
+}
---
+++ app/src/main/java/com/ndm/retrofitapiexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ndm.retrofitapiexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
